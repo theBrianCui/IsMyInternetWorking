d3b89d59b1f34fadbbf36147cc814ae831129ad0
Brian Cui <brian.cui@live.com>
2018-02-05 18:30:34 -0600

    Implemented localStorage api and prev ip

--- src/js/domHelpers.js
+++ src/js/domHelpers.js
@@ -2,4 +2,13 @@ function getByClass(selector) {
     return document.getElementsByClassName(selector);
 }
 
-export { getByClass };
+// shortcut methods for the localStorage API
+// https://developer.mozilla.org/en-US/docs/Web/API/Storage
+const storage = {
+    get: (key) => { return window.localStorage.getItem(key); },
+    set: (key, value) => { return window.localStorage.setItem(key, value); },
+    remove: (key) => { window.localStorage.removeItem(key); },
+    clear: () => { window.localStorage.clear(); }
+}
+
+export { getByClass, storage };
--- src/js/infobox.js
+++ src/js/infobox.js
@@ -1,9 +1,34 @@
-import { getByClass } from "./domHelpers.js";
+import { getByClass, storage } from "./domHelpers.js";
 
-// register each of the info boxes
+// save a reference to the nodes that need updating
+const target_nodes = {
+    ip: getByClass("infobox-content-ip")[0],
+    ip_prev: getByClass("infobox-content-ip-prev")[0],
+    ping: getByClass("infobox-content-ping")[0],
+    location: getByClass("infobox-content-loc")[0],
+    ua: getByClass("infobox-content-ua")[0]
+}
+
+// setters for each of the info boxes
 export default {
-    ip: ((tc) => { getByClass("infobox-content-ip")[0].textContent = tc }),
-    ping: ((tc) => { getByClass("infobox-content-ping")[0].textContent = tc + "ms" }),
+    ip: (function() {
+        var current_ip = null;
+        var prev_ip = storage.get("infobox-prev-ip") || null;
+
+        return (new_ip) => {
+            // do nothing if the current and new IPs are the same
+            // preserve the old prev_ip
+            if (current_ip === new_ip)
+                return;
+
+            target_nodes.ip_prev.textContent = prev_ip = current_ip || prev_ip || "N/A";
+            target_nodes.ip.textContent = current_ip = new_ip;
+
+            storage.set("infobox-prev-ip", current_ip);
+        }
+    })(),
+
+    ping: ((tc) => { target_nodes.ping.textContent = tc + "ms" }),
 
     location: ((location) => {
         var location_string = "";
@@ -21,11 +46,11 @@ export default {
             location_string += ` (${location["latitude"]}, ${location["longitude"]})`;
         }
 
-        getByClass("infobox-content-loc")[0].textContent = location_string;
+        target_nodes.location.textContent = location_string;
     }),
 
     ua: ((tc) => {
-        var ua_node = getByClass("infobox-content-ua")[0];
+        var ua_node = target_nodes.ua;
 
         if (tc.length > 80) {
             ua_node.classList.add("infobox-content-small");
